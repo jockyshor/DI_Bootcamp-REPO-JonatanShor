4b58fd0323ef688b96046c3618ac28cf7a5776bb
Jonatan <jockyshor@gmail.com>
2026-04-28 18:51:17 +0300

    todo list redone

--- Week3/DailyChallenge/todo-list/todo-script.js
+++ Week3/DailyChallenge/todo-list/todo-script.js
@@ -3,25 +3,35 @@ let theTaskList = document.getElementById("unorderedList");
 let addBar = document.getElementById("theAddBar");
 let addButton = document.getElementById("addButton");
 let userInput = document.getElementById("addbar");
+let allTasksEver = {}
 
-function createTask(){
+function createTask(e){
+    e.preventDefault();
     let submitedTask = userInput.value;
+    if (submitedTask === ""){alert("Type Something Before submitting"); return};
+    let taskId = "task_" + Date.now();
+    allTasksEver[taskId] = {
+        name: submitedTask,
+        isDone: false
+    };
+
     let liTask = document.createElement("li");
     let doneBtn = document.createElement("button");
     let deleteBtn = document.createElement("button");
     liTask.textContent = submitedTask;
     doneBtn.className = "btn waves-effect waves-light btn-small right";
     doneBtn.type = "click";
-    doneBtn.addEventListener("click", function(){liTask.style.textDecoration = "line-through" });
+    doneBtn.addEventListener("click", function(){liTask.style.textDecoration = "line-through"; allTasksEver[taskId].isDone = true;});
     doneBtn.innerHTML = "Done";
     liTask.className = "flow-text fontsize-2rem";
     deleteBtn.className = "btn waves-effect waves-light btn-small left";
     deleteBtn.type = "click";
     deleteBtn.addEventListener("click", function(){liTask.remove()})
-    deleteBtn.innerHTML = "Delete";
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
     liTask.appendChild(deleteBtn);
     liTask.appendChild(doneBtn);
-    theTaskList.appendChild(liTask)
+    theTaskList.appendChild(liTask);
+    userInput.value = ""
 }
 
 
